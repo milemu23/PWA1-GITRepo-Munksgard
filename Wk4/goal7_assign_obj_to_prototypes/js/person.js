f271af5ee1e69e9a5e1c06731a8d8ea861c67bad
Michelle Munksgard <design@michellemunksgard.com>
2015-02-27 15:04:19 -0500

    Person Update with Code

--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -27,13 +27,29 @@
         this.name = name;
 
         //randomly select action
+        this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
+        
         //randomly select job
+        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
 
-//pass row number into the constructor
+        //pass row number into the constructor
+        this.row = row;
 
-//main.js writes out the information for person's name and job
+        //main.js writes out the information for person's name and job
 
-//initialize the action - third column
+        //initialize the action - third column by using HTML id
+        var id = document.getElementById("r"+this.row+"c3");
+        id.innerHTML=this.action;
+    }
 
-//person object - update() function - changes the action of the person
-//display in HTML
+    //person object - update() function - changes the action of the       person
+    //display in HTML
+    Person.prototype.update = function(){
+        if (Math.floor(Math.random() <.01)){
+            var i = Math.floor(Math.random()*Person.actions.length);
+            this.action = Person.actions[i];
+            var id = document.getElementById("r"+this.row+"c3");
+            id.innerHTML=this.action;
+        }
+    }
+})();
