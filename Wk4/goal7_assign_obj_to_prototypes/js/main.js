2a4331af34f5713a95eaf03de1e678ce1dedd495
Michelle Munksgard <design@michellemunksgard.com>
2015-02-27 14:23:09 -0500

    Added Pseudocode

---
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,32 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+//Michelle Munksgard
+//February 27, 2015
+// OBJECTS.Person
+
+//create an array = names, store fives names
+var names = ["Michelle", "Jenny", "Krissy", "Daylyn", "Boe"];
+
+//create three instances of the Person object using a for loop
+
+
+//store info in an array called people
+//an array with three pople and their object information
+
+//create person object & randomly select names using Math.random
+
+
+//create variable person to create new person object - row from HTML
+//created using a constructor
+
+//create function = populateHTML - outputs the person's name and job in the DOM
+//do not allow duplicate names
+
+//set up interval that calls runUpdate() function - 30 times a second - ACTION
+//setInterval(runUpdate, 1000 / 30);
+
+//run prototype update() function
---
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -0,0 +1,29 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+
+//Michelle Munksgard
+//February 27, 2015
+// OBJECTS.Person
+
+
+//add Person object to -- main.js needs it
+
+//create two variables = job = with 4 different jobs, actions = 3 different actions
+
+//Person Object - has name, passing from main.js
+//Person constructor is created
+//name -set in this constructor for the Person
+//randomly select action
+//randomly select job
+
+//pass row number into the constructor
+
+//main.js writes out the information for person's name and job
+
+//initialize the action - third column
+
+//person object - update() function - changes the action of the person
+//display in HTML
