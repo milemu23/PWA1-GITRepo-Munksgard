1c049f4b00fbf6e7b106830ab11c1f3b7bc02e11
Michelle Munksgard <design@michellemunksgard.com>
2015-02-27 15:04:38 -0500

    Main.js Code Added

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -8,25 +8,52 @@
 //February 27, 2015
 // OBJECTS.Person
 
-//create an array = names, store fives names
-var names = ["Michelle", "Jenny", "Krissy", "Daylyn", "Boe"];
+//self-executing function
+(function(){
 
-//create three instances of the Person object using a for loop
+    //Define all variables
+    var numOfPeople = 3;
+    var people = [];
+    //create an array = names, store fives names
+    var names = ["Michelle", "Jenny", "Krissy", "Daylyn", "Boe"];
+    var interval;
+    
+
+    //create three instances of the Person object using a for loop
+    for (var i = 0; i < numOfPeople ; i++) {
+        
+        //generate random number from name index
+        var personIndex = Math.floor(Math.random()*names.length);
 
 
-//store info in an array called people
-//an array with three pople and their object information
+        //create person object & randomly select names using Math.random
+        var person = new Person(names[personIndex], i+1);
+        
+        //create function = populateHTML - outputs the person's name and job in the DOM
+        //do not allow duplicate names
+        populateHTML(person.name,"r"+(i+1)+"c1");
+        populateHTML(person.job,"r"+(i+1)+"c2");
+        
+        //allow only one interval at a time
+        clearInterval(interval);
 
-//create person object & randomly select names using Math.random
+        //set up interval that calls runUpdate() function - 30 times a second - ACTION
+        setInterval(runUpdate, 1000 / 30);
+        
+        //call populateHTML function
+        function populateHTML(data, field){
+            var id=document.getElementById(field);
+            id.innerHTML = data;
+        }
+        
 
-
-//create variable person to create new person object - row from HTML
-//created using a constructor
-
-//create function = populateHTML - outputs the person's name and job in the DOM
-//do not allow duplicate names
-
-//set up interval that calls runUpdate() function - 30 times a second - ACTION
-//setInterval(runUpdate, 1000 / 30);
-
-//run prototype update() function
+    //run prototype update() function
+        function runUpdate(){
+            people.forEach(function(element){
+                console.log(element);
+                
+            //update - the prototype methond in person.js
+                element.update();
+            });
+        }
+    })();
